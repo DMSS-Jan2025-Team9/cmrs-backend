e4673e603278dc4a86762e82bc0864bf0576dad7
Chua Jia Qi <81315196+JQChua@users.noreply.github.com>
2025-04-27 00:21:35 +0800

    Use Student Full Id for course registration

--- usermanagement/src/main/java/com/example/usermanagement/repository/StudentRepository.java
+++ usermanagement/src/main/java/com/example/usermanagement/repository/StudentRepository.java
@@ -5,10 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;   
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
     List<Student> findByJobId(String jobId);  // Find students by jobId
+
+    Optional<Student> findBystudentFullId(String studentFullId);
 
     List<Student> findByProgramName(String programName);
 }
